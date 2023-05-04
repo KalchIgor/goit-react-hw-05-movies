169086fa78e01b493ff1e0f16d65a78a2205a552
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-05-04 13:17:36 +0300

    api

---
+++ src/shared/Api/Api.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3/";
+const KEY = "c01f14dcdb58e9cec669b1017a4d540c";
+
+export const getTrendingMovies = async () => {
+    const query = `trending/movie/day?api_key=${KEY}`;
+    const { data: movies } = await axios.get(query);
+    return movies;
+};
+
+export const searchMovies = async (searchParam) => {
+    const query = `search/movie?api_key=${KEY}&page=1&query=${searchParam}`;
+    const { data: movies } = await axios.get(query);
+    return movies;
+};
+
+export const getMovieDetails = async (movieId) => {
+    const query = `movie/${movieId}$?api_key=${KEY}`;
+    const { data: movie } = await axios.get(query);
+    return movie;
+};
+
+
+export const getMovieCast = async (movieId) => {
+    const query = `movie/${movieId}/credits?api_key=${KEY}`;
+    const { data } = await axios.get(query);
+    return data;
+};
+
+export const getReviews = async (movieId) => {
+    const query = `movie/${movieId}/reviews?api_key=${KEY}`;
+    const { data } = await axios.get(query);
+    return data;
+};
