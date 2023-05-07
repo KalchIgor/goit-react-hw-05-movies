b505f0b7759f8fb619a264e9bf09d5c14b2a2928
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-05-07 10:33:16 +0300

    ButtonBack.

---
+++ src/pages/ButtonBack/ButtonBack.jsx
@@ -0,0 +1,13 @@
+import { Link, useLocation } from "react-router-dom";
+import css from "./ButtonBack.module.css";
+
+export const ButtonBack=()=> {
+    const location=useLocation();
+    const backLink=location.state?.from ?? '/';
+
+    return (<div>
+        <Link to={backLink} className={css.link} >Go back</Link>
+           </div>)
+}
+
+;
--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,9 +1,10 @@
-import { Outlet, useParams, Link, useLocation } from "react-router-dom"
-import { useState, useEffect, Suspense } from "react"
+import { Outlet, useParams, Link, useLocation} from "react-router-dom";
+import { useState, useEffect, Suspense } from "react";
 import { getMovieDetails } from "shared/Api/Api";
 import { Loader } from "components/Loader/Loader";
 import css from "./MovieDetails.module.css";
 import noImg from "..//..//img/No-picture-available.png";
+import { ButtonBack }  from 'pages/ButtonBack/ButtonBack';
 
 
 export default function MovieDetails() {
@@ -11,107 +12,101 @@ export default function MovieDetails() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-const { movieId } = useParams();
+  const location = useLocation();
+  const { movieId } = useParams();
+  
+  useEffect(() => {
+    const movieDetails = async () => {
+      try {
+        setLoading(true);
+        setError(null);
 
-const location = useLocation();
-const from = location.state?.from || "/"
-
-useEffect(() => {
-  const movieDetails = async () => {
-    try {
-      setLoading(true);
-      setError(null);
-
-      const result = await getMovieDetails(movieId);
-      setMovie(result);
-    } catch(e) {
-      setError(e);
-    } finally {
-      setLoading(false);
+        const result = await getMovieDetails(movieId);
+        setMovie(result);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
     }
-  }
-  movieDetails();
-}, [movieId])
+    movieDetails();
+  }, [movieId])
 
+  const isCastDetails = location.pathname.includes("cast");
+  const castLink = isCastDetails ? `/movies/${movieId}` : `/movies/${movieId}/cast`;
+  const isReviewDetails = location.pathname.includes("reviews");
+  const reviewLink = isReviewDetails ? `/movies/${movieId}` : `/movies/${movieId}/reviews`;
+  const from = location.state?.from ?? "/";
 
-const isCastDetails = location.pathname.includes("cast");
-const castLink = isCastDetails ? `/movies/${movieId}` : `/movies/${movieId}/cast`;
-const isReviewDetails = location.pathname.includes("reviews");
-const reviewLink = isReviewDetails ? `/movies/${movieId}` : `/movies/${movieId}/reviews`;
-  
-const backLink = location.state?.from ?? '/movies';
-
-    return (
-      <div className={css.container}>
-
-        <Link to={backLink}  className={ css.link } >Go back</Link>
-       
-        {loading && <Loader />}
-        {error && <p>Something went wrong</p>}
-        {movie && (
+  return (
+    <div className={css.container}>
+       <ButtonBack />
+      {loading && <Loader />}
+      {error && <p>Something went wrong</p>}
+      {movie && (
         <div className={css.movieDetails}>
-            <div className={css.boxImg}>
-              <img className={css.img} src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : noImg} alt="poster"/>
-            </div>
-            <div className={css.boxInfo}>
-              <h2>{movie.title}</h2>
-              <p>User Score: {`${(movie.vote_average * 10).toFixed(1)}`}%</p>
-              <h3>Overview</h3>
-              <p>{`${movie.overview}`}</p>
-              <h3>Genres</h3>
-              <p>{`${movie.genres.map(genre => genre.name).join(' / ')}`}</p>
-              <div className={css.boxAddInfo}>
+          <div className={css.boxImg}>
+            <img className={css.img} src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : noImg} alt="poster" />
+          </div>
+          <div className={css.boxInfo}>
+            <h2>{movie.title}</h2>
+            <p>User Score: {`${(movie.vote_average * 10).toFixed(1)}`}%</p>
+            <h3>Overview</h3>
+            <p>{`${movie.overview}`}</p>
+            <h3>Genres</h3>
+            <p>{`${movie.genres.map(genre => genre.name).join(' / ')}`}</p>
+            <div className={css.boxAddInfo}>
               <h2>Additional information</h2>
-              <Link className={css.castLink} state={{from}} to={castLink} replace>Cast</Link>
-              <Link className={css.reviewLink} state={{from}} to={reviewLink} replace>Reviews</Link>
+              <Link className={css.castLink} state={{ from }} to={castLink} replace>Cast</Link>
+              <Link className={css.reviewLink} state={{ from}} to={reviewLink} replace>Reviews</Link>
             </div>
-            </div>
+          </div>
         </div>
-        )}
-        <Suspense fallback={<Loader />}>
-        <Outlet/> 
-        </Suspense>
-      </div>
-    )
-  }
-  
+      )}
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </div>
+  )
+}
 
-  /*
-  const goBack = () => navigate(-1);
+
+/*
+const goBack = () => navigate(-1);
 const isCastDetails = location.pathname.includes("cast");
 const castLink = isCastDetails ? `/movies/${movieId}` : `/movies/${movieId}/cast`;
 const isReviewDetails = location.pathname.includes("reviews");
 const reviewLink = isReviewDetails ? `/movies/${movieId}` : `/movies/${movieId}/reviews`;
 
-    return (
-      <div className={css.container}>
-        <button className={css.button} onClick={goBack}>Go back</button>
-        {loading && <Loader />}
-        {error && <p>Something went wrong</p>}
-        {movie && (
-        <div className={css.movieDetails}>
-            <div className={css.boxImg}>
-              <img className={css.img} src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : noImg} alt="poster"/>
-            </div>
-            <div className={css.boxInfo}>
-              <h2>{movie.title}</h2>
-              <p>User Score: {`${(movie.vote_average * 10).toFixed(1)}`}%</p>
-              <h3>Overview</h3>
-              <p>{`${movie.overview}`}</p>
-              <h3>Genres</h3>
-              <p>{`${movie.genres.map(genre => genre.name).join(' / ')}`}</p>
-              <div className={css.boxAddInfo}>
-              <h2>Additional information</h2>
-              <Link className={css.castLink} state={{from}} to={castLink} replace>Cast</Link>
-              <Link className={css.reviewLink} state={{from}} to={reviewLink} replace>Reviews</Link>
-            </div>
-            </div>
-        </div>
-        )}
-        <Suspense fallback={<Loader />}>
-        <Outlet/> 
-        </Suspense>
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={goBack}>Go back</button>
+      {loading && <Loader />}
+      {error && <p>Something went wrong</p>}
+      {movie && (
+      <div className={css.movieDetails}>
+          <div className={css.boxImg}>
+            <img className={css.img} src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : noImg} alt="poster"/>
+          </div>
+          <div className={css.boxInfo}>
+            <h2>{movie.title}</h2>
+            <p>User Score: {`${(movie.vote_average * 10).toFixed(1)}`}%</p>
+            <h3>Overview</h3>
+            <p>{`${movie.overview}`}</p>
+            <h3>Genres</h3>
+            <p>{`${movie.genres.map(genre => genre.name).join(' / ')}`}</p>
+            <div className={css.boxAddInfo}>
+            <h2>Additional information</h2>
+            <Link className={css.castLink} state={{from}} to={castLink} replace>Cast</Link>
+            <Link className={css.reviewLink} state={{from}} to={reviewLink} replace>Reviews</Link>
+          </div>
+          </div>
       </div>
-    )
-  }
-  */
+      )}
+      <Suspense fallback={<Loader />}>
+      <Outlet/> 
+      </Suspense>
+    </div>
+  )
+} 
+*/
